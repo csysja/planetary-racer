81843b107f1542c0f17d64f6024df38918ed644f
Stephen Allatt <stephen.allatt@gmail.com>
2019-09-17 22:37:27 +0100

    make it work

--- ship.js
+++ ship.js
@@ -5,10 +5,23 @@ const quadTopLeft = 1;
 const quadTopRight = 2;
 const quadBottomRight = 3;
 const quadBottomLeft = 4;
+const maxSpeed = 3;
+let mutateMultiplier = 1;
 
+function mutate(mutateMultiplier) {
+	return function(x) {
+		if (random(1) < (0.2 * mutateMultiplier)) {
+			let offset = randomGaussian() * 0.4 * mutateMultiplier;
+			let newx = x + offset;
+			return newx;
+		} else {
+			return x;
+		}
+	}
+}
 
 class Ship {
-  constructor(x, y, midpoint, innerCircleRadius, outerCircleRadius) {
+  constructor(x, y, midpoint, innerCircleRadius, outerCircleRadius, canvasSize, brain, mutateMultiplier) {
     this.x = x;
 		this.y = y;
 		this.velocityX = 0;
@@ -18,13 +31,27 @@ class Ship {
 		this.midpoint = midpoint;
 		this.innerCircleRadius = innerCircleRadius;
 		this.outerCircleRadius = outerCircleRadius;
+		this.canvasSize = canvasSize;
 		this.currentQuad = this.getCurrentQuad();
 		this.previousQuad = this.currentQuad;
-		this.brain = new NeuralNetwork();
+		this.fitness = 0;
+		this.hasEverMoved = false;
+		this.isAlive = true;
+
+		if (brain instanceof NeuralNetwork) {
+      this.brain = brain.copy();
+			this.brain.mutate(mutate(mutateMultiplier));
+		} else {
+			this.brain = new NeuralNetwork(5, 10, 2);
+		}
+
 	}
 	
 	left() {
 		this.angle -= angularSpeed;
+		if (this.angle < 0) {
+			this.angle += Math.PI * 2;	// always want positive angle
+		}
 	}
 	
 	right() {
@@ -34,6 +61,13 @@ class Ship {
 	forward() {
 		this.velocityX += Math.cos(this.angle - halfPi) * acceleration;
 		this.velocityY += Math.sin(this.angle - halfPi) * acceleration;		
+		const magnitude = Math.sqrt(Math.pow(this.velocityX, 2) + Math.pow(this.velocityY, 2));	// calculate magnitude of two vectors
+		if (magnitude > maxSpeed) {
+			const factor = maxSpeed / magnitude;	// ship going too fast, calculate how much to slow it by
+			this.velocityX *= factor;
+			this.velocityY *= factor;
+		}
+		this.hasEverMoved = true;
 	}
 
 	animate() {
@@ -42,21 +76,69 @@ class Ship {
 		// recognise which quadrant ship is in
 		this.previousQuad = this.currentQuad;
 		this.currentQuad = this.getCurrentQuad();
+		this.useBrain();	
+		this.forward();
+		this.calculateFitness();
+	}
+
+	calculateFitness() {
+		if (this.hasEverMoved && this.isAlive) {
+			this.fitness += 1;
+		}
+		if (this.isNewQuad() && !this.isCollide()) {
+			this.fitness += 1000;
+		}
+	}
+
+	isNewQuad() {
+		return this.previousQuad !== this.currentQuad;
+	}
+
+	roundDecimal(number, numDecimalPlaces) {
+		return Math.round(number * Math.pow(10, numDecimalPlaces)) / Math.pow(10, numDecimalPlaces);
+	}
+
+	useBrain() {
+		const x = this.x - this.midpoint;
+		const y = this.canvasSize - this.y - this.midpoint;
+		const angleToCenter = Math.atan2(y, -x);
+		const angleRelativeToX = angleToCenter >= 0 ? angleToCenter : 2 * Math.PI + angleToCenter;
+		const shipAngleCos = Math.cos(this.angle);
+		const shipAngleSin = Math.cos(this.angle);
+		const shipAngleRelativeToXCos = Math.cos(angleRelativeToX);
+		const shipAngleRelativeToXSin = Math.sin(angleRelativeToX);
+		let inputs = [];
+		const distance = Math.sqrt(Math.pow(this.x - this.midpoint, 2) + Math.pow(this.y - this.midpoint, 2))
+		const normalisedDistance = map(distance, this.innerCircleRadius, this.outerCircleRadius, 0, 1);
+		inputs.push(normalisedDistance);
+		inputs.push(map(shipAngleCos, -1, 1, 0, 1));
+		inputs.push(map(shipAngleSin, -1, 1, 0, 1));
+		inputs.push(map(shipAngleRelativeToXCos, -1, 1, 0, 1));
+		inputs.push(map(shipAngleRelativeToXSin, -1, 1, 0, 1));
+		const action = this.brain.predict(inputs);
+		if (action[0] > 0.5) {
+			this.left();
+		}
+		if (action[1] > 0.5) {
+			this.right();
+		}	
 	}
 
 	isCollide() {
 		const distance = this.distanceBetweenPoints(this.x, this.y, this.midpoint, this.midpoint);
 		if (distance < this.innerCircleRadius) {
+			this.isAlive = false;
 			return true;
 		}
 		if (distance > this.outerCircleRadius) {
+			this.isAlive = false;
 			return true;
 		}
-		console.log(`${this.currentQuad} ${this.previousQuad}`);
 		// don't allow ship to return to previous quadrant
 		if (this.currentQuad < this.previousQuad && 
 				(this.previousQuad !== 4 || this.currentQuad !== 1) ||
 				this.previousQuad === 1 && this.currentQuad === 4) {
+			this.isAlive = false;
 			return true;
 		}
 		return false;
